bb41f7f2576b877157f747fda591cc6bdd4949b6
Raoul v. R <vanruesc@outlook.de>
2016-05-15 23:09:22 +0200

    Added documentation.

---
+++ docs/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Octant",
+        "Octree",
+        "OctreeHelper",
+        "Raycasting"
+    ],
+    "modules": [
+        "octree"
+    ],
+    "allModules": [
+        {
+            "displayName": "octree",
+            "name": "octree",
+            "description": "Exposure of the library components."
+        }
+    ],
+    "elements": []
+} };
+});
