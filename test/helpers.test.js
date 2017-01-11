dd51842f558b5b1acafb297fff0b80b68b8bba6e
Raoul v. R <vanruesc@outlook.de>
2017-01-11 21:55:17 +0100

    Moved a lost test case into the correct suite.

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -18,6 +18,19 @@ module.exports = {
 
 		},
 
+		"creates geometry for each tree level": function(test) {
+
+			const octree = new lib.Octree(box.min, box.max);
+
+			octree.root.split();
+
+			const helper = new lib.OctreeHelper(octree);
+
+			test.equal(helper.children.length, 2, "should have a child for each level");
+			test.done();
+
+		},
+
 		"can be destroyed": function(test) {
 
 			const octree = new lib.Octree(box.min, box.max);
