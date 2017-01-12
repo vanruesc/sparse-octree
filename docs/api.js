c99515145fd3b356621ef031b7e05c849e8c75d6
Raoul v. R <vanruesc@outlook.de>
2017-01-12 20:43:12 +0100

    Updated docs.

--- docs/api.js
+++ docs/api.js
@@ -6,6 +6,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "Octant",
         "Octree",
         "OctreeHelper",
+        "OctreeIterator",
         "OctreeRaycaster",
         "PointOctant",
         "PointOctree",
