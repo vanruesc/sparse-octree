3766b84db30822dcffac6623b3f865cd4844bea3
Raoul v. R <vanruesc@users.noreply.github.com>
2018-07-02 00:13:09 +0200

    Changed the build setup.

---
+++ rollup.config.js
@@ -0,0 +1,73 @@
+import babel from "rollup-plugin-babel";
+import minify from "rollup-plugin-babel-minify";
+import resolve from "rollup-plugin-node-resolve";
+
+const pkg = require("./package.json");
+const date = (new Date()).toDateString();
+
+const banner = `/**
+ * ${pkg.name} v${pkg.version} build ${date}
+ * ${pkg.homepage}
+ * Copyright ${date.slice(-4)} ${pkg.author.name}, ${pkg.license}
+ */`;
+
+const lib = {
+
+	input: pkg.module,
+	output: {
+		file: "build/" + pkg.name + ".js",
+		format: "umd",
+		name: pkg.name.replace(/-/g, "").toUpperCase(),
+		banner: banner,
+		globals: { three: "THREE" }
+	},
+
+	external: ["three"],
+	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
+
+};
+
+const demo = {
+
+	input: "demo/src/index.js",
+	output: {
+		file: "public/demo/index.js",
+		format: "iife",
+		globals: { three: "THREE" }
+	},
+
+	external: ["three"],
+	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
+
+};
+
+export default [lib, demo].concat((process.env.NODE_ENV === "production") ? [
+
+	Object.assign({}, lib, {
+
+		output: Object.assign({}, lib.output, {
+			file: "build/" + pkg.name + ".min.js"
+		}),
+
+		plugins: [resolve(), babel(), minify({
+			bannerNewLine: true,
+			sourceMap: false,
+			comments: false
+		})]
+
+	}),
+
+	Object.assign({}, demo, {
+
+		output: Object.assign({}, demo.output, {
+			file: "public/demo/index.min.js"
+		}),
+
+		plugins: [resolve(), babel(), minify({
+			sourceMap: false,
+			comments: false
+		})]
+
+	})
+
+] : []);
