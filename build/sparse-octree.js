dcc85e69e8a90f92cfb9d480e62d509d9d0308a4
Raoul v. R <vanruesc@users.noreply.github.com>
2018-12-01 17:10:41 +0100

    Fresh build.

--- build/sparse-octree.js
+++ build/sparse-octree.js
@@ -1,5 +1,5 @@
 /**
- * sparse-octree v5.0.2 build Wed Oct 31 2018
+ * sparse-octree v5.0.2 build Sat Dec 01 2018
  * https://github.com/vanruesc/sparse-octree
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
--- public/demo/index.js
+++ public/demo/index.js
@@ -3168,7 +3168,6 @@
   var change = new DemoManagerEvent("change");
   var load = new DemoManagerEvent("load");
 
-  var initialHash = window.location.hash.slice(1);
   var DemoManager = function (_EventTarget) {
     _inherits(DemoManager, _EventTarget);
 
@@ -3238,36 +3237,33 @@
       value: function loadDemo() {
         var _this3 = this;
 
-        var id = this.demo;
-        var demos = this.demos;
-        var demo = demos.get(id);
-        var previousDemo = this.currentDemo;
+        var nextDemo = this.demos.get(this.demo);
+        var currentDemo = this.currentDemo;
         var renderer = this.renderer;
-        window.location.hash = id;
+        window.location.hash = nextDemo.id;
 
-        if (previousDemo !== null) {
-          previousDemo.reset();
+        if (currentDemo !== null) {
+          currentDemo.reset();
         }
 
         this.menu.domElement.style.display = "none";
-        renderer.clear();
-        change.previousDemo = previousDemo;
-        change.demo = demo;
-        this.currentDemo = demo;
+        change.previousDemo = currentDemo;
+        change.demo = nextDemo;
+        this.currentDemo = nextDemo;
         this.dispatchEvent(change);
-        demo.load().then(function () {
-          return _this3.startDemo(demo);
-        }).catch(function (e) {
-          return console.error(e);
-        });
+        renderer.clear();
+        nextDemo.load().then(function () {
+          return _this3.startDemo(nextDemo);
+        }).catch(console.error);
       }
     }, {
       key: "addDemo",
       value: function addDemo(demo) {
+        var hash = window.location.hash.slice(1);
         var currentDemo = this.currentDemo;
         this.demos.set(demo.id, demo.setRenderer(this.renderer));
 
-        if (this.demo === null || demo.id === initialHash) {
+        if (this.demo === null && hash.length === 0 || demo.id === hash) {
           this.demo = demo.id;
           this.loadDemo();
         }
@@ -3310,8 +3306,18 @@
         this.renderer.setSize(width, height);
 
         if (demo !== null && demo.camera !== null) {
-          demo.camera.aspect = width / height;
-          demo.camera.updateProjectionMatrix();
+          var camera = demo.camera;
+
+          if (camera instanceof three.OrthographicCamera) {
+            camera.left = width / -2.0;
+            camera.right = width / 2.0;
+            camera.top = height / 2.0;
+            camera.bottom = height / -2.0;
+            camera.updateProjectionMatrix();
+          } else if (!(camera instanceof three.CubeCamera)) {
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+          }
         }
       }
     }, {
