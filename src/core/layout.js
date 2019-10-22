2a319688dcddef8c71dfe2881360b5da489d8c28
Raoul v. R <vanruesc@users.noreply.github.com>
2019-10-22 14:38:32 +0200

    Create layout.js

---
+++ src/core/layout.js
@@ -0,0 +1,28 @@
+/**
+ * A binary pattern that describes the standard octant layout:
+ *
+ * ```text
+ *    3____7
+ *  2/___6/|
+ *  | 1__|_5
+ *  0/___4/
+ * ```
+ *
+ * This common layout is crucial for positional assumptions.
+ *
+ * @type {Uint8Array[]}
+ */
+
+export const layout = [
+
+	new Uint8Array([0, 0, 0]),
+	new Uint8Array([0, 0, 1]),
+	new Uint8Array([0, 1, 0]),
+	new Uint8Array([0, 1, 1]),
+
+	new Uint8Array([1, 0, 0]),
+	new Uint8Array([1, 0, 1]),
+	new Uint8Array([1, 1, 0]),
+	new Uint8Array([1, 1, 1])
+
+];
