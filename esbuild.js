1b23da566c1a8d176b33ad1b4d4bf4d553f1895c
Raoul v. R <vanruesc@users.noreply.github.com>
2021-04-01 14:59:43 +0200

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -1,16 +1,12 @@
 import { createRequire } from "module";
-import { hideBin } from "yargs/helpers";
 import esbuild from "esbuild";
-import yargs from "yargs";
 
 const require = createRequire(import.meta.url);
 const pkg = require("./package");
 const date = (new Date()).toDateString();
-const production = (process.env.NODE_ENV === "production");
-const external = Object.keys(pkg.peerDependencies);
-const { argv } = yargs(hideBin(process.argv))
-	.option("watch", { alias: "w", type: "boolean" });
-
+const external = Object.keys(pkg.peerDependencies || {});
+const minify = (process.env.NODE_ENV === "production");
+const watch = process.argv.includes("-w");
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
  * ${pkg.homepage}
@@ -18,27 +14,22 @@ const banner = `/**
  * @license ${pkg.license}
  */`;
 
-const common = {
-	logLevel: "info",
-	bundle: true
-};
-
-const configs = [{
-	entryPoints: ["demo/src/index.ts"],
-	outfile: "public/demo/index.js",
-	format: "iife",
-	minify: production,
-	watch: argv.watch
-}, {
+await esbuild.build({
 	entryPoints: ["src/index.ts"],
 	outfile: `dist/${pkg.name}.js`,
 	banner: { js: banner },
+	logLevel: "info",
 	format: "esm",
+	bundle: true,
 	external
-}];
+}).catch(() => process.exit(1));
 
-for(const c of configs) {
-
-	void esbuild.build(Object.assign(c, common));
-
-}
+await esbuild.build({
+	entryPoints: ["demo/src/index.ts"],
+	outfile: "public/demo/index.js",
+	logLevel: "info",
+	format: "iife",
+	bundle: true,
+	minify,
+	watch
+}).catch(() => process.exit(1));
