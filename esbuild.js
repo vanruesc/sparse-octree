6dacbbf9138bb55d850ae4c79a1af27ed8556c81
Raoul v. R <vanruesc@users.noreply.github.com>
2021-04-23 14:31:31 +0200

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -4,9 +4,6 @@ import esbuild from "esbuild";
 const require = createRequire(import.meta.url);
 const pkg = require("./package");
 const date = (new Date()).toDateString();
-const external = Object.keys(pkg.peerDependencies || {});
-const minify = (process.env.NODE_ENV === "production");
-const watch = process.argv.includes("-w");
 const banner = `/**
  * ${pkg.name} v${pkg.version} build ${date}
  * ${pkg.homepage}
@@ -17,19 +14,19 @@ const banner = `/**
 await esbuild.build({
 	entryPoints: ["src/index.ts"],
 	outfile: `dist/${pkg.name}.js`,
+	external: Object.keys(pkg.peerDependencies || {}),
 	banner: { js: banner },
 	logLevel: "info",
 	format: "esm",
-	bundle: true,
-	external
+	bundle: true
 }).catch(() => process.exit(1));
 
 await esbuild.build({
 	entryPoints: ["demo/src/index.ts"],
 	outfile: "public/demo/index.js",
+	minify: process.argv.includes("-m"),
+	watch: process.argv.includes("-w"),
 	logLevel: "info",
 	format: "iife",
-	bundle: true,
-	minify,
-	watch
+	bundle: true
 }).catch(() => process.exit(1));
