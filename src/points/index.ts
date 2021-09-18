0e45212c69c96a69995b7d7b0717b692ed2d4844
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-18 16:01:23 +0200

    Update index.ts

--- src/points/index.ts
+++ src/points/index.ts
@@ -2,3 +2,4 @@ export { PointContainer } from "./PointContainer";
 export { PointData } from "./PointData";
 export { PointOctant } from "./PointOctant";
 export { PointOctree } from "./PointOctree";
+export { RayPointIntersection } from "./RayPointIntersection";
