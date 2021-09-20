f22c60323b51524bf640f621af9f647748404d79
Raoul v. R <vanruesc@users.noreply.github.com>
2021-09-20 13:51:50 +0200

    Update OctreeRaycaster.js

--- test/core/OctreeRaycaster.js
+++ test/core/OctreeRaycaster.js
@@ -11,15 +11,15 @@ test("can find intersecting octants", t => {
 
 	const octree = new Octree(root);
 	const raycaster = new Raycaster(
-		new Vector3(0.5, -1, 0.5),
-		new Vector3(1, 1, 1)
+		new Vector3(0.5, 0.5, 2),
+		new Vector3(0, 0, -1)
 	);
 
 	octree.root.split();
 
 	const intersects = octree.getIntersectingNodes(raycaster);
 
-	t.is(intersects.length, 1, "should return one intersecting octant");
-	t.is(intersects[0], octree.root.children[5], "should return the sixth child");
+	t.is(intersects.length, 2, "should return two intersecting octant");
+	t.is(intersects[0], octree.root.children[7], "should return octant #8");
 
 });
